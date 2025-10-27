fcf9d875a29453f1b5d8444a15198d18e11876fd
Brian Uceda <monalek0622@hotmail.com>
2025-10-27 10:01:05 -0500

    fix: prueba con grid

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { MotiView } from "moti"
+import { MotiView } from "moti";
 
 import "./global.css";
 
@@ -12,11 +12,11 @@ export default function App() {
       <Text className="w-full py-3 text-center android:text-blue-400 ios:text-green-400 font-semibold text-lg">
         React 19 + Expo 54 + NativeWind v4!
       </Text>
-      <View className="flex-row flex-wrap content-start p-3 gap-3">
+      <View className="grid grid-cols-2 p-3 gap-3">
         {Array.from({ length: 6 }).map((_, index) => (
           <View
             key={index}
-            className="h-32 w-[48%] flex-grow rounded-md border bg-blue-400/20 ios:bg-green-400/20 border-blue-400/40 ios:border-green-400/40"
+            className="h-32 rounded-md border bg-blue-400/20 ios:bg-green-400/20 border-blue-400/40 ios:border-green-400/40"
           />
         ))}
       </View>
@@ -24,4 +24,3 @@ export default function App() {
     </SafeAreaView>
   );
 }
-
