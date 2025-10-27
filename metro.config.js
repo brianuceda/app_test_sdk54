e69ff341a73cfdc659c4956ce2c4c0fa2626b621
Brian Uceda <monalek0622@hotmail.com>
2025-10-27 12:55:30 -0500

    test

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,17 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 const path = require("path");
 
+console.log("Loading metro config...");
+
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { 
+console.log("Base config loaded");
+console.log("Config:", JSON.stringify(config, null, 2));
+
+const finalConfig = withNativeWind(config, { 
   input: path.resolve(__dirname, "./global.css") 
 });
+
+console.log("NativeWind config applied");
+
+module.exports = finalConfig;
