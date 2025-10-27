e4b1910fbcf701f1864470f4044e4634ee8e54bb
Brian Uceda <monalek0622@hotmail.com>
2025-10-27 10:12:08 -0500

    fix: cambio arreglado

--- metro.config.cjs
+++ metro.config.cjs
@@ -1,6 +1,13 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+import { getDefaultConfig } from "expo/metro-config";
+import { withNativeWind } from "nativewind/metro";
+
+// Get the project root directory
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+export default withNativeWind(config, { input: "./global.css" });
