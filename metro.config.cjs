a69a086d47142c34495144654be136855632b98c
Brian Uceda <monalek0622@hotmail.com>
2025-10-27 12:30:40 -0500

    fix: change to commonjs

--- metro.config.cjs
+++ metro.config.cjs
@@ -1,13 +1,6 @@
-import { getDefaultConfig } from "expo/metro-config";
-import { withNativeWind } from "nativewind/metro";
-
-// Get the project root directory
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-export default withNativeWind(config, { input: "./global.css" });
+module.exports = withNativeWind(config, { input: "./global.css" });
